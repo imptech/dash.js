cc77115bdb53e69ad3749e335122bb513001c9f5
whatvn <hungnv@opensource.com.vn>
2015-12-30 19:43:22 +0700

    Add Width and Height to expose bitrate full info before DashJS 2.0 realease

--- src/dash/extensions/DashManifestExtensions.js
+++ src/dash/extensions/DashManifestExtensions.js
@@ -352,7 +352,12 @@ Dash.dependencies.DashManifestExtensions.prototype = {
             bitrateList = [];
 
         for (var i = 0; i < ln; i += 1) {
-            bitrateList.push(reps[i].bandwidth);
+            //bitrateList.push(reps[i].bandwidth);
+            bitrateList.push({
+                bandwidth:reps[i].bandwidth,
+                width:reps[i].width || 0,
+                height:reps[i].height || 0
+            });
         }
 
         return bitrateList;
--- src/streaming/controllers/AbrController.js
+++ src/streaming/controllers/AbrController.js
@@ -341,7 +341,10 @@ MediaPlayer.dependencies.AbrController = function () {
                 bitrateInfo = new MediaPlayer.vo.BitrateInfo();
                 bitrateInfo.mediaType = type;
                 bitrateInfo.qualityIndex = i;
-                bitrateInfo.bitrate = bitrateList[i];
+                //bitrateInfo.bitrate = bitrateList[i];
+                bitrateInfo.bitrate = bitrateList[i].bandwidth;
+                bitrateInfo.width = bitrateList[i].width;
+                bitrateInfo.height = bitrateList[i].height;
                 infoList.push(bitrateInfo);
             }
 
